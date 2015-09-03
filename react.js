262b98978f5d37cacaeaa39162a3df8f41929881
Keith Cirkel <github@keithcirkel.co.uk>
2015-09-03 17:02:48 +0100

    Add new rules from eslint-plugin-react 3.3.0

--- react.js
+++ react.js
@@ -19,18 +19,32 @@ module.exports = {
         ],
         // Enforce boolean attributes notation in JSX
         'react/jsx-boolean-value': 2,
+        // Validate closing bracket location in JSX
+        'react/jsx-closing-bracket-location': [
+            2,
+            {
+                // ensure closing bracket aligns to the opening tag
+                location: 'tag-aligned',
+            },
+        ],
         // Enforce or disallow spaces inside of curly braces in JSX attributes.
         'react/jsx-curly-spacing': [
             2,
             // Spaces are not allowed ever
             'never',
         ],
+        // Validate props indentation in JSX
+        'react/jsx-indent-props': [
+            2,
+            // indent by 2 spaces
+            2,
+        ],
         // Limit maximum of props on a single line in JSX
         'react/jsx-max-props-per-line': [
             2,
             {
-                maximum: 3
-            }
+                maximum: 3,
+            },
         ],
         // Prevent duplicate properties in JSX
         'react/jsx-no-duplicate-props': 2,
@@ -56,6 +70,8 @@ module.exports = {
         'react/no-did-update-set-state': 2,
         // Prevent multiple component definition per file
         'react/no-multi-comp': 2,
+        // Prevent usage of setState
+        'react/no-set-state': 0,
         // Prevent usage of unknown DOM property
         'react/no-unknown-property': 2,
         // Prevent missing props validation in a React component definition
