e3cbcf5af2ba3eccdf38612e473c002568d98f77
Keith Cirkel <keithamus@users.noreply.github.com>
2016-10-12 17:14:41 +0100

    feat: add new rules from eslint-plugin-react 5-6.4
    BREAKING CHANGE:
    
    Old code may cause errors with this new configuration

--- react.js
+++ react.js
@@ -19,6 +19,13 @@ module.exports = {
         'ignoreTranspilerName': false,
       },
     ],
+    // Forbid certain props on Components
+    'react/forbid-component-props': [
+      2,
+      {
+        'forbid': [ 'className', 'style' ],
+      },
+    ],
     // Forbid certain propTypes
     'react/forbid-prop-types': [
       2,
@@ -42,6 +49,26 @@ module.exports = {
       // Spaces are not allowed ever
       'never',
     ],
+    // Enforce or disallow spaces around equal signs in JSX attributes.
+    'react/jsx-equals-spacing': [
+      2,
+      // Spaces are not allowed ever
+      'never',
+    ],
+    // Restrict file extensions that may contain JSX
+    'react/jsx-filename-extension': [
+      2,
+      // Spaces are not allowed ever
+      {
+        'extensions': '.js',
+      },
+    ],
+    // Configure the position of the first property
+    'react/jsx-first-prop-new-line': [
+      2,
+      // Spaces are not allowed ever
+      'multiline',
+    ],
     // Enforce event handler naming conventions in JSX
     'react/jsx-handler-names': [
       2,
@@ -52,6 +79,12 @@ module.exports = {
     ],
     // Validate props indentation in JSX
     'react/jsx-indent-props': [
+      2,
+      // indent by 2 spaces
+      2,
+    ],
+    // Validate JSX indentation
+    'react/jsx-indent': [
       2,
       // indent by 2 spaces
       2,
@@ -69,16 +102,23 @@ module.exports = {
     ],
     // No .bind() or Arrow Functions in JSX Props
     'react/jsx-no-bind': 2,
+    // Prevent comments from being inserted as text nodes
+    'react/jsx-no-comment-textnodes': 2,
     // Prevent duplicate properties in JSX
     'react/jsx-no-duplicate-props': 2,
     // Prevent usage of unwrapped JSX strings
     'react/jsx-no-literals': 0,
+    // Prevent usage of unsafe target='_blank'
+    'react/jsx-no-target-blank': 2,
     // Disallow undeclared variables in JSX
     'react/jsx-no-undef': 2,
     // Enforce PasalCase for user-defined JSX components
-    'react/jsx-pascal-case': 2,
-    // Enforce propTypes declarations alphabetical sorting
-    'react/sort-prop-types': 0,
+    'react/jsx-pascal-case': [
+      2,
+      {
+        'allowAllCaps': true,
+      },
+    ],
     // Enforce props alphabetical sorting
     'react/jsx-sort-props': 0,
     // Validate spacing before closing bracket in JSX
@@ -90,8 +130,16 @@ module.exports = {
     'react/jsx-uses-react': 2,
     // Prevent variables used in JSX to be incorrectly marked as unused
     'react/jsx-uses-vars': 2,
+    // Prevent missing parentheses around multiline JSX
+    'react/jsx-wrap-multilines': 2,
+    // Prevent passing of children as props
+    'react/no-children-prop': 2,
+    // Prevent problem with children and props.dangerouslySetInnerHTML
+    'react/no-danger-with-children': 2,
     // Prevent usage of dangerous JSX properties
     'react/no-danger': 2,
+    // Prevent usage of deprecated methods
+    'react/no-deprecated': 2,
     // Prevent usage of setState in componentDidMount
     'react/no-did-mount-set-state': [
       2,
@@ -104,6 +152,10 @@ module.exports = {
     ],
     // Prevent direct mutation of this.state
     'react/no-direct-mutation-state': 2,
+    // Prevent usage of findDOMNode
+    'react/no-find-dom-node': 2,
+    // Prevent usage of isMounted
+    'react/no-is-mounted': 2,
     // Prevent multiple component definition per file
     'react/no-multi-comp': [
       2,
@@ -113,23 +165,47 @@ module.exports = {
         'ignoreStateless': true,
       },
     ],
+    // Prevent usage of the return value of React.render
+    'react/no-render-return-value': 2,
     // Prevent usage of setState
     'react/no-set-state': 0,
+    // Prevent using string references
+    'react/no-string-refs': 2,
+    // Prevent invalid characters from appearing in markup
+    'react/no-unescaped-entities': 2,
     // Prevent usage of unknown DOM property
     'react/no-unknown-property': 2,
+    // Prevent definitions of unused prop types
+    'react/no-unused-prop-types': 2,
+    // Enforce ES5 or ES6 class for React Components
+    'react/prefer-es6-class': [
+      2,
+      // Always use ES6 classes over ES5 ones
+      'always',
+    ],
     // Enforce stateless React Components to be written as a pure function
     'react/prefer-stateless-function': 2,
     // Prevent missing props validation in a React component definition
     'react/prop-types': 2,
     // Prevent missing React when using JSX
     'react/react-in-jsx-scope': 2,
-    // Restrict file extensions that may be required
-    'react/require-extension': 0,
+    // Enforce React components to have a shouldComponentUpdate method
+    'react/require-optimization': 2,
+    // Enforce ES5 or ES6 class for returning value in render function
+    'react/require-render-return': 2,
     // Prevent extra closing tags for components without children
-    'react/self-closing-comp': 2,
+    'react/self-closing-comp': [
+      2,
+      {
+        'component': true,
+        'html': true,
+      },
+    ],
     // Enforce component methods order
     'react/sort-comp': 2,
-    // Prevent missing parentheses around multiline JSX
-    'react/wrap-multilines': 2,
+    // Enforce propTypes declarations alphabetical sorting
+    'react/sort-prop-types': 0,
+    // Enforce style prop value being an object
+    'react/style-prop-object': 2,
   },
 };
